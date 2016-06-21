6a8717a457a2e05dca24d66de91336c62275eccf
toneloke <david.guerrero87@live.com>
2016-06-20 20:12:20 -0600

    ui-router

--- js/main.js
+++ js/main.js
@@ -1,29 +1,6 @@
 angular.module('Concierge', [])
   .controller('introController', introController)
 
-function introController() {
-  var introCtrl = this;
-};
-
-$(".hover").mouseleave(
-  function () {
-    $(this).removeClass("hover");
-  }
-  // $function() {
-  //   var pull        = $('#pull');
-  //       menu        = $('nav ul');
-  //       menuHeight  = menu.height();
-  //
-  //   $(pull).on('click', function(e) {
-  //     e.preventDefault();
-  //     menu.slideToggle();
-  //   });
-  //
-  //   $(window).resize(function() {
-  //     var w = $(window).width();
-  //     if(w > 320 && menu.is)(':hidden')) {
-  //          menu.removeAttr('style');
-  //     }
-  //   });
-  // });
-);
+function introController () {
+  var introCtrl = this
+}
