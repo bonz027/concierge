0b57ee85ba24567cd4f4d9b94c698d0602158944
toneloke <david.guerrero87@live.com>
2016-06-20 20:49:04 -0600

    ui-router

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-angular.module('Concierge', [])
-  .controller('introController', introController)
+console.log("Concierge Module is loaded!!!!!")
 
-function introController () {
-  var introCtrl = this
+angular.module('Concierge', ['ui.router'])
+  // UI ROUTER CONFIGURATION
+  .config(function routerConfig($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('landing',{
+        url: '/',
+        templateUrl: 'partials/landing.html'
+      })
+      .state('home',{
+        url: '/home',
+        templateUrl: 'partials/home.html',
+        controller: 'homeCtrl as hCtrl'
+      })
+    $urlRouterProvider.otherwise('/')
+  })
+  .controller('homeCtrl', homeCtrl)
+
+function homeCtrl () {
+  var hCtrl = this
 }
